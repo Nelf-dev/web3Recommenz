3ab6ecef7f7eb61bee91e9b2a6024287cb975b01
Nelson Feng <nelsonfeng98@gmail.com>
2023-12-17 17:35:17 +1100

    Add weight and local data set into local model

--- challenge2-incremental/incremental_client.py
+++ challenge2-incremental/incremental_client.py
@@ -6,6 +6,7 @@ import pickle
 import base64
 from incremental_model import AutoEncoderTrainer, AE, reset_seeds #replace AutoEncoderTrainer to Trainer Leo will make
 import pdb
+import oldDataSet
 # import spreadsheet
 
 server_location = "http://localhost:4001"
@@ -18,7 +19,20 @@ def post_server_data(params, segment, loss, epochs):
     data = {"params": params, "segment": segment, "loss": loss, "epochs": epochs}
     requests.post(server_location+"/post_data", json=data)
 
+def get_dataset(segment):
+    # Transforms images to a PyTorch Tensor
+    # MAY OR MAY NOT USE THIS
+    #tensor_transform = transforms.ToTensor()
 
+    trainset = oldDataSet
+
+    loader = torch.utils.data.DataLoader(
+        trainset,
+        batch_size=16,
+        shuffle=False,
+        num_workers=2
+        )
+    return loader
 
 def main():
     epoch = 0
@@ -30,7 +44,7 @@ def main():
         # model = AE() #Change this to Leos Model
         # serialized_data, epochs = get_server_data()
         # state_dict = pickle.loads(base64.b64decode(serialized_data))
-        # model.load_state_dict(state_dict)
+        # model.load_state_dict(state_dict) #This is the loading weights function
         # loss = AutoEncoderTrainer.train(model, 1, segment)
         # post_server_data(base64.b64encode(pickle.dumps(model.state_dict())).decode("utf-8"), segment, loss, epochs)
         # print("Client:",client_id, "Epochs:", epochs, "Loss:", loss, "Segment: "+sys.argv[1])
@@ -46,19 +60,20 @@ def main():
         if epochs == 0:
             weights = serialized_data
 
-        # subModel1Updates = Send these weights and OLD local dataset to submodel1
-        # model = AE()
-        # state_dict = pickle.loads(base64.b64decode(serialized_data))
-        # model.load_state_dict(state_dict)
+        model = AE()
 
-        # loss = AutoEncoderTrainer.train(model, 1, segment)
+        #convert decode serialized data
+        state_dict = pickle.loads(base64.b64decode(serialized_data))
+        #load weights
+        model.load_state_dict(state_dict)
+        # Send data set to model
+        dataset = get_dataset(segment)
+        loss = AutoEncoderTrainer.train(model, 1, segment, dataset)
+        # not sure how to just send noisy local model weights updates
+        post_server_data(base64.b64encode(pickle.dumps(model.state_dict())).decode("utf-8"), segment, loss, epochs)
 
-
-        # # send subModel1Updates -  post_server_data(subModel1Updates)
-        # post_server_data(base64.b64encode(pickle.dumps(model.state_dict())).decode("utf-8"), segment, loss, epochs)
-
-        # # Wait ten seconds
-        # time.sleep(10)
+        # Wait ten seconds
+        time.sleep(10)
 
         # serialized_data, epochs = get_server_data()
         # subModel2Updates = send completed JSON data to ARFED submodel
