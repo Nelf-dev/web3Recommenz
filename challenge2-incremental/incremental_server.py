56335d3882f1d697ca5eb468bedb5eb1dbefed24
Ho <Corey.Ho@team.telstra.com>
2023-12-17 15:11:47 +1100

    Added initial params

--- challenge2-incremental/incremental_server.py
+++ challenge2-incremental/incremental_server.py
@@ -5,7 +5,7 @@ Challenge 2: Incremental Learning
 from pyhypercycle_aim import SimpleServer, JSONResponseCORS, aim_uri
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
-from incremental_model import AutoEncoderTrainer
+# from incremental_model import generic_model
 import os
 
 PORT = os.environ.get("PORT", 4001)
@@ -16,49 +16,57 @@ class IncrementalExample(SimpleServer):
                 "version": "0.1",
                 "license": "MIT",
                 "author": "HyperCycle"
-              }
+               }
     def __init__(self):
         pass
 
     @aim_uri(uri="/get_data", methods=["GET"],
-            endpoint_manifest = {
-                "input_query": "",
-                "input_headers": "",
-                "output": {},
-                "documentation": "Returns the parameters of this model, along with the number of epochs computed so far.",
-                "example_calls": [{
-                    "body": "",
-                    "method": "GET",
-                    "query": "",
-                    "headers": "",
-                    "output": {"epochs":3,"params": "gASVgQAAAAAAAACMC2Nv..."}
-                }]
-            })
+             endpoint_manifest = {
+                 "input_query": "",
+                 "input_headers": "",
+                 "output": {},
+                 "documentation": "Returns the parameters of this model, along with the number of epochs computed so far.",
+                 "example_calls": [{
+                     "body": "",
+                     "method": "GET",
+                     "query": "",
+                     "headers": "",
+                     "output": {"epochs":3,"params": "gASVgQAAAAAAAACMC2Nv..."}
+                 }]
+             })
     def get_data(self, request):
         client_id = request.query_params.get("client_id")
-        params, epochs = AutoEncoderTrainer.get_data()
+
+        if epochs == 0:
+            params, epochs = 0000, 0000
+        else:
+        # Assuming that the generic_model.get_data() return a tuple    
+        # params, epochs = generic_model.get_data()
+            
+        params, epochs = 9999, 9999
         data = {"params": params, "epochs": epochs}
         
         return JSONResponseCORS(data)
 
     @aim_uri(uri="/post_data", methods=["POST"],
-            endpoint_manifest = {
-                "input_query": "",
-                "input_headers": "",
-                "output": {},
-                "documentation": "",
-                "example_calls": [{
-                    "body": {"params": "gASVgQAAAAAAAACMC2Nv...", "segement": 1,
+             endpoint_manifest = {
+                 "input_query": "",
+                 "input_headers": "",
+                 "output": {},
+                 "documentation": "",
+                 "example_calls": [{
+                     "body": {"params": "gASVgQAAAAAAAACMC2Nv...", "segement": 1,
                               "loss": 0.6894218985755133, "epochs": 3},
-                    "method": "POST",
-                    "query": "",
-                    "headers": "",
-                    "output": {"updated": True}
-                }]
-            })
+                     "method": "POST",
+                     "query": "",
+                     "headers": "",
+                     "output": {"updated": True}
+                 }]
+             })
     async def post_data(self, request):
         data = await request.json()
-        result = AutoEncoderTrainer.post_data(data['params'], data['segment'], data['loss'], data['epochs'])
+        # result = generic_model.post_data(data['params'], data['loss'], data['epochs'])
+        result = 1111
         return JSONResponseCORS({"updated": result})
 
 def main():
