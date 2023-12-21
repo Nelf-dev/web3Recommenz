eac87dd5d5c274bf2a5fd967f98d518725b3c524
interuniverse <corey.hkk@gmail.com>
2023-12-21 20:11:51 +1100

    Updated incremental_model.py with submodel_three and submodel_four

--- challenge2-incremental/incremental_model.py
+++ challenge2-incremental/incremental_model.py
@@ -16,11 +16,13 @@ import pdb
 import random
 from collections import OrderedDict
 from typing import List
+from river import preprocessing
+from river.anomaly import HalfSpaceTrees
+
 
 # TODO: Nelson needs this import to download NLTK MODULE
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
-
 
 # Download nltk module
 nltk.download('punkt')
@@ -57,20 +59,10 @@ def generate_sentence(model, max_length=20):
             break
     return ' '.join(sentence)
 
-def submodel_zero(dataset):
-    captions = dataset['Caption'].tolist()
-    markov_model = build_markov_model(captions)
-
-    synthesized_captions = []
-    for _ in range(5):
-        synthesized_captions.append(generate_sentence(markov_model))
-
-    return pd.DataFrame(synthesized_captions, columns=['Caption'])
-
 # Function to check and balance the dataset
 def balance_dataset(df):
     sentiment_counts = df['Sentiment'].value_counts()
-    if any(sentiment_counts != sentiment_counts[0]):
+    if len(sentiment_counts) > 0 and any(sentiment_counts != sentiment_counts.iloc[0]):
         ros = RandomOverSampler(random_state=42)
         df_balanced, _ = ros.fit_resample(df, df['Sentiment'])
         return df_balanced
@@ -128,7 +120,6 @@ class SentimentAnalysisModel_global(nn.Module):
 
     def forward(self, x):
         x = self.relu(self.fc1(x))
-        # x = self.batchnorm(x)
         x = self.groupnorm(x)
         x = self.dropout(x)
         x = self.fc2(x)
@@ -236,5 +227,165 @@ def submodel_two(models: List[OrderedDict]) -> OrderedDict:
             averages[new_key] = sums[key] / counts[key].clamp(min=1)  # Avoid division by zero
 
     return averages
+
+def submodel_three(dataset, seed=13):   # RANDOM SEEDS 13 & 18 GIVES SYNTHESIZED CAPTIONS WITH SENTIMENT = GOOD
+    # Set the random seed for reproducibility
+    random.seed(seed)
+
+    captions = dataset['Caption'].tolist()
+    markov_model = build_markov_model(captions)
+
+    synthesized_captions = []
+    for _ in range(5):
+        synthesized_captions.append(generate_sentence(markov_model))
+
+    return pd.DataFrame(synthesized_captions, columns=['Caption'])
+
+
+def submodel_four(global_parameters_path, synth_local_text_df, node1_incremental_1_df):
+    # Adjusting sequences to the fixed input size of 100
+    # Assuming each word vector from Word2Vec is of size 100
+    vector_size = 100
+    fixed_input_size = 100
+    modified_input_size = fixed_input_size * vector_size  # 10000
+    
+    # Load model with pretrained parameters
+    model = SentimentAnalysisModel_global(input_size=modified_input_size)
+    model.load_state_dict(torch.load(global_parameters_path))
+    model.eval()
+
+    # Step 1: Predict sentiment for synth_local_text_df
+    synth_local_text_df['processed_caption'] = synth_local_text_df['Caption'].apply(preprocess_text)
+
+    # Create a Word2Vec model from all sentences
+    sentences = synth_local_text_df['processed_caption'].tolist()
+    word2vec_model = create_embeddings(sentences)
+
+    # Convert sentences to sequences of vectors
+    vectorized_synth_local = [[word2vec_model.wv[word] for word in sentence if word in word2vec_model.wv] for sentence in sentences]
+
+    # Pad or truncate the sequences
+    adjusted_sequences = pad_or_truncate_sequences(vectorized_synth_local, fixed_input_size, vector_size)
+    adjusted_sequences = adjusted_sequences.reshape(len(adjusted_sequences), -1)
+
+    # Convert to tensor and predict
+    synth_local_tensor = torch.tensor(adjusted_sequences, dtype=torch.float32)
+    with torch.no_grad():
+        predictions = model(synth_local_tensor)
+        predicted_labels = torch.argmax(predictions, dim=1)
+
+    synth_local_text_df['Local_sentiment'] = predicted_labels.numpy()
+    pred_synth_local_text_df = synth_local_text_df[['Caption', 'Local_sentiment']]
+
+    # Step 2: Create recommend_df
+    recommend_df = pred_synth_local_text_df[pred_synth_local_text_df['Local_sentiment'] == 1]  # Assuming 1 corresponds to 'good'
+
+    # Convert numeric Local_sentiment values back to string labels
+    sentiment_label_mapping = {0: 'bad', 1: 'good', 2: 'neutral'}
+    bgn_recommend_df = recommend_df.copy()
+    bgn_recommend_df['Local_sentiment'] = bgn_recommend_df['Local_sentiment'].replace(sentiment_label_mapping)
+
+    # Step 3: Create local_node1_incremental_1_df
+    local_node1_incremental_1_df = node1_incremental_1_df[['Caption', 'Sentiment']].rename(columns={'Sentiment': 'Local_sentiment'})
+
+    # Step 4: Outlier detection
+    scaler = preprocessing.StandardScaler()
+    hst = HalfSpaceTrees()
+
+    # Convert sentiment labels to numeric if they are not already
+    sentiment_mapping = {'good': 1, 'bad': 0, 'neutral': 2}
+    pred_synth_local_text_df['Local_sentiment'] = pred_synth_local_text_df['Local_sentiment'].replace(sentiment_mapping)
+
+    numerical_features = pred_synth_local_text_df[['Local_sentiment']]
+
+    # Train the outlier detector
+    for _, row in numerical_features.iterrows():
+        row_dict = row.to_dict()
+        scaler.learn_one(row_dict)
+        vec = scaler.transform_one(row_dict)
+        hst.learn_one(vec)
+
+    # Prepare the data from local_node1_incremental_1_df for outlier detection
+    local_node1_incremental_1_df['Local_sentiment'] = local_node1_incremental_1_df['Local_sentiment'].replace(sentiment_mapping)
+    new_row = local_node1_incremental_1_df[['Local_sentiment']].iloc[0].to_dict()
+    scaler.learn_one(new_row)  # Update the scaler with the new data
+    new_row_scaled = scaler.transform_one(new_row)
+    is_outlier = hst.score_one(new_row_scaled) > 0.5  # Threshold can be adjusted
+
+    if not is_outlier:
+        df_input = pd.concat([pred_synth_local_text_df, local_node1_incremental_1_df])
+    else:
+        df_input = pred_synth_local_text_df
+
+    # After outlier detection, ensure the column name expected by balance_dataset is present
+    if 'Sentiment' not in df_input.columns and 'Local_sentiment' in df_input.columns:
+        df_input.rename(columns={'Local_sentiment': 'Sentiment'}, inplace=True)
+
+    # Convert numeric Sentiment values back to string labels
+    bgn_df_input = df_input.copy()
+    bgn_df_input['Sentiment'] = bgn_df_input['Sentiment'].replace(sentiment_label_mapping)
+
+    # Step 1: Check and balance dataset
+    df = balance_dataset(df_input)
+
+    # Step 2: Preprocess text data
+    df['processed_caption'] = df['Caption'].apply(preprocess_text)
+    sentences = df['processed_caption'].tolist()
+    word2vec_model = create_embeddings(sentences)
+
+    # Convert sentences to sequences of vectors
+    vectorized_synth_local = [[word2vec_model.wv[word] for word in sentence if word in word2vec_model.wv] for sentence in sentences]
+
+    # Now pad or truncate the sequences
+    adjusted_sequences = pad_or_truncate_sequences(vectorized_synth_local, fixed_input_size, vector_size)
+
+    # Flatten the sequences for input to the neural network
+    adjusted_sequences = adjusted_sequences.reshape(len(adjusted_sequences), -1)
+
+    # Target variable is one-hot encoded and needs to be converted to class indices
+    # Convert target variable from one-hot encoded to class indices
+    target = df['Sentiment'].factorize()[0]
+
+    # Split dataset
+    X_train, X_test, y_train, y_test = train_test_split(adjusted_sequences, target, test_size=0.25, random_state=33)
+
+    # Convert to PyTorch tensors
+    X_train_tensor = torch.tensor(X_train, dtype=torch.float32)
+    y_train_tensor = torch.tensor(y_train, dtype=torch.long)  # Ensure this is long type for CrossEntropyLoss
+    X_test_tensor = torch.tensor(X_test, dtype=torch.float32)
+    y_test_tensor = torch.tensor(y_test, dtype=torch.long)
+
+    # DataLoader
+    train_data = TensorDataset(X_train_tensor, y_train_tensor)
+    train_loader = DataLoader(train_data, batch_size=64)
+
+    # Load model with pretrained parameters
+    model = SentimentAnalysisModel_global(input_size=modified_input_size)
+    model.load_state_dict(torch.load(global_parameters_path))
+    model.train()
+
+    # Step 3: Train model with differential privacy
+    optimizer = optim.Adam(model.parameters(), lr=0.1)
+    criterion = nn.CrossEntropyLoss()
+    privacy_engine = PrivacyEngine()
+    model, optimizer, train_loader = privacy_engine.make_private(
+        module=model,
+        optimizer=optimizer,
+        data_loader=train_loader,
+        noise_multiplier=1.0,
+        max_grad_norm=1.0
+    )
+
+    # Training loop
+    for epoch in range(10):
+        for inputs, targets in train_loader:
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+            loss.backward()
+            optimizer.step()
+
+    # Return updated model parameters
+    return bgn_recommend_df, bgn_df_input, model.state_dict()
 
 
